17b1228c58b202ae48cfa4d959605f0531b28d93
Moein <64212565+moeinpourhamzeh@users.noreply.github.com>
2024-11-06 17:44:01 +0330

    init

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D _rb;
+
+    private float _horizontalMovement;
+    private float speed = 5f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _horizontalMovement = Input.GetAxisRaw("Horizontal");
+
+        if (_horizontalMovement != 0)
+        {
+            _rb.AddForce(new Vector2(_horizontalMovement * speed, 0f));
+        }
+    }
+}
